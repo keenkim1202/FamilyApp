25eae4f5ecb764da0700092139e7ba1698d2c696
KEEN <homurahomu@naver.com>
2021-03-05 17:11:57 +0900

    searchController를 통해 검색어를 입력하면 filter된 목록이 뜨도록 구현

--- FamilyApp/Sources/Service/UserRepository.swift
+++ FamilyApp/Sources/Service/UserRepository.swift
@@ -9,7 +9,35 @@ import Foundation
 
 protocol UserRepositoryType {
   var count: Int { get }
-  func user() -> UserType?
+  func user(indexRow: Int) -> UserType?
   func add(userType: UserType)
   func fetch() -> [UserType]
+//  func update()
+//  func remove()
+}
+
+class UserRepository: UserRepositoryType {
+  private var items: [UserType]
+  
+  init() {
+    items = []
+  }
+  
+  var count: Int {
+    return items.count
+  }
+  
+  func add(userType: UserType) {
+    items.append(userType)
+  }
+  
+  func user(indexRow: Int) -> UserType? {
+    guard indexRow < count else { return nil }
+    return items[indexRow]
+  }
+  
+  func fetch() -> [UserType] {
+    return []
+  }
+
 }
---
+++ FamilyApp/Sources/Shared/FamilyApp++Bool.swift
@@ -0,0 +1,14 @@
+//
+//  FamilyApp++Bool.swift
+//  FamilyApp
+//
+//  Created by KEEN on 2021/03/05.
+//
+
+import Foundation
+
+extension Bool {
+  var negate: Bool {
+    return !self
+  }
+}
