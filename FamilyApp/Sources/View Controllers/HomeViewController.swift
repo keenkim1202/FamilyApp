f86b63194ab3f7810ca409a354fe84a9e1525796
KEEN <homurahomu@naver.com>
2021-03-05 15:12:24 +0900

    Member.swift를 UserType.swift로 이름 수정

--- FamilyApp/Sources/View Controllers/HomeViewController.swift
+++ FamilyApp/Sources/View Controllers/HomeViewController.swift
@@ -9,25 +9,32 @@ import UIKit
 
 class HomeViewController: UIViewController {
   
-  var memberType: Member?
+  // TODO : searchBar 연결해서 검색 되도록 하기
+  // TODO : 매세지 목록 탭 화면 구성하기
   
-  var dummyList: [Member] = [
-    Member(name: "kate", state: "공부중", thumbnail: UIImage(named: "defaultProfile")),
-    Member(name: "keen", state: "휴식중", thumbnail: UIImage(named: "defaultProfile"))]
+  var userType: UserType?
+  var searchWord: String? = nil
+  let searchController: UISearchController = UISearchController()
+
+  var dummyList: [UserType] = [
+    UserType(name: "kate", state: "공부중", thumbnail: UIImage(named: "defaultProfile")),
+    UserType(name: "keen", state: "휴식중", thumbnail: UIImage(named: "defaultProfile"))]
   
   @IBOutlet weak var tableView: UITableView!
-  @IBOutlet weak var searchBar: UISearchBar!
+
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    searchController.searchBar.placeholder = "이름으로 검색을 할 수 있습니다."
+    navigationItem.searchController = searchController
   }
-}
 
+}
 
 // MARK: TableView Delegate & DataSource
 extension HomeViewController: UITableViewDelegate {
   func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-      return 85
+    return 85
   }
 }
 
@@ -48,19 +55,19 @@ extension HomeViewController: UITableViewDataSource {
     cell.alertButton.layer.cornerRadius = CGFloat(8)
     cell.messageButton.backgroundColor = UIColor(named: "primaryBlue")
     cell.messageButton.layer.cornerRadius = CGFloat(8)
-
+    
     return cell
   }
 }
 
-// MARK: SearchBar Delegate
-extension HomeViewController: UISearchBarDelegate {
-  func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-    searchBar.endEditing(true)
+// MARK: UISearchResultsUpdating
+extension HomeViewController: UISearchResultsUpdating {
+  func updateSearchResults(for searchController: UISearchController) {
+    let searchWord = searchController.searchBar.text
+    
+    guard self.searchWord != searchWord else { return }
+//    filtered = (searchWord?.isEmpty ?? true).negate
+    
+    tableView.reloadData()
   }
-//  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
-//    searchBar.endEditing(true)
-//  }
-
 }
-
